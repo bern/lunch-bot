79b5f3d2d5cd9af5a1e7a40c5a56777cfa6650b0
Cerek Hillen <cerekh@gmail.com>
2020-01-15 10:05:50 -0500

    Impl test help

--- tests/lib/handlers/test_help.py
+++ tests/lib/handlers/test_help.py
@@ -1,3 +1,13 @@
-def test_help_success():
-    # TODO (#1): Implement test
-    pass
+from lib.handlers.help import HELP_MESSAGE
+from lib.handlers.help import handle_help
+
+
+def test_help_success(mock_client, mock_storage, mock_send_reply, make_zulip_message):
+    message, args = make_zulip_message("help")
+    handle_help(
+        mock_client, mock_storage, message, args,
+    )
+
+    mock_send_reply.assert_called_with(
+        mock_client, message, HELP_MESSAGE,
+    )
