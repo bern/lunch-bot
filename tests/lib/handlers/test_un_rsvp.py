c9809123c3454c59272a6f8eeb7ce1e3950db05a
Cerek Hillen <cerekh@gmail.com>
2020-01-27 14:36:26 -0500

    impl unrsvp

--- tests/lib/handlers/test_un_rsvp.py
+++ tests/lib/handlers/test_un_rsvp.py
@@ -83,6 +83,59 @@ def test_handle_un_rsvp_not_rsvpd(
     )
 
 
+def test_handle_un_rsvp_ambiguous(
+    mock_client, mock_storage, mock_send_reply, make_zulip_message, make_time
+):
+    """
+    Ensures that, when the user has provided an ambiguous query, handle_un_rsvp
+    prompts them to disambiguate.
+    """
+    plan1 = Plan("tjs", make_time(11, 00), [])
+    plan2 = Plan("tjs", make_time(12, 30), [])
+    mock_storage.get.return_value = {
+        plan1.uuid: plan1,
+        plan2.uuid: plan2,
+    }
+
+    message, args = make_zulip_message("un-rsvp tjs")
+    handle_un_rsvp(mock_client, mock_storage, message, args)
+
+    mock_storage.put.assert_not_called()
+    mock_send_reply.assert_called_with(
+        mock_client,
+        message,
+        """There are multiple lunches with that lunch_id. Please reissue the command with the time of the lunch you're interested in:
+tjs @ 11:00am
+tjs @ 12:30pm""",
+    )
+
+
+def test_handle_un_rsvp_disambiguate(
+    mock_client, mock_storage, mock_send_reply, make_zulip_message, make_time
+):
+    """
+    Ensures that, when the user has provided an ambiguous query, handle_un_rsvp
+    prompts them to disambiguate.
+    """
+    plan1 = Plan("tjs", make_time(11, 00), [])
+    plan2 = Plan("tjs", make_time(12, 30), [User("Test Sender", 5678)])
+    mock_storage.get.return_value = {
+        plan1.uuid: plan1,
+        plan2.uuid: plan2,
+    }
+
+    message, args = make_zulip_message("un-rsvp tjs 12:30")
+    handle_un_rsvp(mock_client, mock_storage, message, args)
+
+    mock_storage.put.assert_called_with(
+        mock_storage.PLANS_ENTRY,
+        {plan1.uuid: plan1, plan2.uuid: Plan("tjs", make_time(12, 30), []),},
+    )
+    mock_send_reply.assert_called_with(
+        mock_client, message, "You've successful un-RSVP'd to lunch at tjs."
+    )
+
+
 def test_handle_un_rsvp_success(
     mock_client, mock_storage, mock_send_reply, make_zulip_message, make_time
 ):
@@ -95,7 +148,7 @@ def test_handle_un_rsvp_success(
         plan.uuid: plan,
     }
 
-    message, args = make_zulip_message("un-rsvp 0")
+    message, args = make_zulip_message("un-rsvp tjs")
     handle_un_rsvp(mock_client, mock_storage, message, args)
 
     mock_storage.put_assert_called_with(
