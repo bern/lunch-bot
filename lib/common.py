bcf9262d802b84e61bb9d7920f7a362a3ddc1d24
Cerek Hillen <cerekh@gmail.com>
2020-01-26 22:35:24 -0500

    Impl min edit distance. Will be useful later

--- lib/common.py
+++ lib/common.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from collections import defaultdict
+from typing import Callable
+from typing import Dict
 import zulip
 
 from lib.models.message import Message
@@ -37,3 +40,34 @@ def get_now() -> datetime:
     function only exists because it can be mocked, but datetime.now() cannot.
     """
     return datetime.now()
+
+
+def min_edit_distance(
+    source: str,
+    target: str,
+    insert_cost: Callable[[str], int] = lambda char: 1,
+    delete_cost: Callable[[str], int] = lambda char: 1,
+    replace_cost: Callable[[str, str], int] = lambda source_char, target_char: 0
+    if source_char == target_char
+    else 2,
+) -> int:
+    """
+    Returns the minimum edit distance from between two strings. Allows the user
+    to customize the functions used to calculate insert, delete, and replacement
+    costs.
+    """
+    costs: Dict[int, Dict[int, int]] = defaultdict(lambda: defaultdict(lambda: 0))
+
+    for i in range(len(source)):
+        costs[i + 1][0] = costs[i][0] + delete_cost(source[i])
+    for j in range(len(target)):
+        costs[0][j + 1] = costs[0][j] + insert_cost(target[j])
+
+    for j, target_char in enumerate(target):
+        for i, source_char in enumerate(source):
+            costs[i + 1][j + 1] = min(
+                costs[i][j + 1] + delete_cost(target_char),
+                costs[i + 1][j] + insert_cost(source_char),
+                costs[i][j] + replace_cost(source_char, target_char),
+            )
+    return costs[len(source)][len(target)]
--- tests/lib/test_common.py
+++ tests/lib/test_common.py
@@ -1,3 +1,4 @@
+from lib.common import min_edit_distance
 from lib.common import send_reply
 
 
@@ -15,3 +16,49 @@ def test_send_reply(mock_client, make_zulip_message):
     mock_client.send_message.assert_called_with(
         {"type": "private", "to": ["tester@email.com",], "content": "Here is a reply!",}
     )
+
+
+def test_min_edit_distance_insert():
+    """
+    Tests that the insert cost functions correctly. Forced to insert by making
+    the source string empty.
+    """
+    assert 1234 == min_edit_distance("", "a", insert_cost=lambda char: 1234)
+
+
+def test_min_edit_distance_delete():
+    """
+    Tests that the delete cost functions correctly. Forced to delete by making
+    the target string empty.
+    """
+    assert 1234 == min_edit_distance("a", "", delete_cost=lambda char: 1234)
+
+
+def test_min_edit_distance_replace():
+    """
+    Tests that the replace cost functions correctly.
+    """
+    assert 1234 == min_edit_distance(
+        "a",
+        "b",
+        insert_cost=lambda char: 5000,
+        delete_cost=lambda char: 5000,
+        replace_cost=lambda source_char, target_char: 1234,
+    )
+
+
+def test_min_edit_distance_equal():
+    """
+    Ensures that when two strings are the same, the min edit distance metric has
+    a cost of 0.
+    """
+    assert 0 == min_edit_distance("same_string", "same_string")
+    assert 0 == min_edit_distance("abc", "abc")
+
+
+def test_min_edit_distance():
+    """
+    General test cases for min edit distance between two words.
+    """
+    assert 1 == min_edit_distance("cat", "cats")
+    assert 4 == min_edit_distance("door", "gore")
